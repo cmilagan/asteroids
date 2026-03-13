6d73afbc1b369faf0cfeb67b585a44e36aa33daa
Christian Ilagan <cmilagan7@gmail.com>
2026-03-13 16:29:30 +1100

    Initialise player and circle objects

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,6 +9,10 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 # delta time in seconds
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # game loop
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,7 +20,10 @@ def main():
                 pygame.quit()
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000.0
 
 
 if __name__ == "__main__":
